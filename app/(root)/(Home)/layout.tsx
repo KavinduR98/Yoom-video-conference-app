feaae366cd387f2144a7ef95fe7eab287a0d7945
@kavinduushan07 <kavinduushan07@gmail.com>
2024-04-21 15:14:42 +0530

    add toast messages

--- app/(root)/(Home)/layout.tsx
+++ app/(root)/(Home)/layout.tsx
@@ -1,6 +1,7 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
 import React, { ReactNode } from 'react'
 
+
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
     <main>
